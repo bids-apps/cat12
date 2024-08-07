0b126274857bcdaa9ae09de3bae99e13ed53909e
Remi Gau <remi_gau@hotmail.com>
2024-08-07 10:52:00 +0200

    Apply suggestions from code review
    Co-authored-by: sourcery-ai[bot] <58596630+sourcery-ai[bot]@users.noreply.github.com>

--- code/cat_logging.py
+++ code/cat_logging.py
@@ -9,7 +9,8 @@ from rich.logging import RichHandler
 from rich.traceback import install
 
 
-def cat12_log(name: str | None = None) -> logging.Logger:
+def cat12_log(name: str | None = None, log_level: int = logging.INFO) -> logging.Logger:
+    """Create log."""
     """Create log."""
     # let rich print the traceback
     install(show_locals=True)
