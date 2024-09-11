33360e0ecaa160770c8d2ce936d051ef91e9d2a7
Remi Gau <remi_gau@hotmail.com>
2024-09-11 16:53:33 +0200

    add doc

---
+++ docs/source/conf.py
@@ -0,0 +1,35 @@
+"""Conf for documentation."""
+
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = "cat12"
+copyright = "2024, cat12"
+author = "cat12"
+release = "0.1.0"
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ["_templates"]
+exclude_patterns = []
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "pydata_sphinx_theme"
+html_static_path = ["_static"]
+
+extensions = [
+    "sphinx_copybutton",
+    "sphinx_togglebutton",
+    "myst_parser",
+    "sphinxarg.ext",
+]
---
+++ docs/source/index.rst
@@ -0,0 +1,13 @@
+Welcome to cat12 BIDS app documentation!
+========================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+Command line API
+----------------
+
+.. argparse::
+   :ref: cat12._parsers.common_parser
+   :prog: cat12
